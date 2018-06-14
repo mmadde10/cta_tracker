fe48164e34679a57349bd8228d7f880a9ce8019c
Mark Madden <markmadden@Marks-Mac-mini.local>
2018-06-13 23:19:46 -0500

    HTTP Requests working

--- lib/arrival_data.dart
+++ lib/arrival_data.dart
@@ -6,38 +6,37 @@ import 'package:http/http.dart' as http;
 
   String mapid = '40710';
   String max = '5';
-
+//'http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx' + '?key=' + properties['ctakey'] + '&mapid='+ mapid + '&max=' + max + '&outputType=JSON',
 Future<Arrival> fetchArrival() async {
-  final response = await http.get(
-    'http://lapi.transitchicago.com/api/1.0/ttarrivals.aspx' + '?key=' + properties['ctakey'] + '&mapid='+ mapid + '&max=' + max + '&outputType=JSON',
-  );
+  final response = await http.get('');
   final responseJson = json.decode(response.body);
   return new Arrival.fromJson(responseJson);
 }
 
 class Arrival {
 
-  int stopId;
-  String stationName;
-  String stopDestination;
-  String routeColor;
-  String destinationName;
-  DateTime arrivalTime;
-  double latitude;
-  double longitude;
+  dynamic stopId;
+  dynamic stationName;
+  dynamic stopDestination;
+  dynamic routeColor;
+  dynamic destinationName;
+  dynamic arrivalTime;
+  dynamic latitude;
+  dynamic longitude;
 
   Arrival({this.stopId, this.stationName, this.stopDestination, this.routeColor, this.destinationName, this.arrivalTime, this.latitude, this.longitude});
 
   factory Arrival.fromJson(Map<String, dynamic> json) {
+    Map<int, dynamic> responseMap = json['ctatt']['eta'].asMap();
     return new Arrival(
-      stopId: json['ctatt']['eta']['stpId'],
-      stationName: json['ctatt']['eta']['staNm'],
-      stopDestination: json['ctatt']['eta']['stpDe'],
-      routeColor: json['ctatt']['eta']['rt'],
-      destinationName: json['ctatt']['eta']['destNm'],
-      arrivalTime: json['ctatt']['eta']['arrT'],
-      latitude: json['ctatt']['eta']['lat'],
-      longitude: json['ctatt']['eta']['lon']
+      stopId: responseMap[0]['stpId'],
+      stationName: responseMap[0]['staNm'],
+      stopDestination: responseMap[0]['stpDe'],
+      routeColor: responseMap[0]['rt'],
+      destinationName: responseMap[0]['destNm'],
+      arrivalTime: responseMap[0]['arrT'],
+      latitude: responseMap[0]['lat'],
+      longitude: responseMap[0]['lon']
     );
   }
 }
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -15,15 +15,40 @@ class HomePage extends StatelessWidget {
       appBar: new AppBar(
         title: new Text(title),
       ),
-      body: new Center(
+      body: new Container(
         child: new FutureBuilder<Arrival>(
         future: fetchArrival(),
         builder: (context,snapshot){
           if(snapshot.hasData){
-            return new Text((snapshot.data).toString());
+            return new GridView.count(
+                primary: true,
+                padding: const EdgeInsets.all(20.0),
+                crossAxisSpacing: 10.0,
+                crossAxisCount: 2,
+                children: <Widget>[
+                   new Card(
+                    child: new Container(
+                      child: new Column(
+                        children: <Widget>[
+                          new Text((snapshot.data.routeColor)),
+                          new Text((snapshot.data.destinationName)),
+                          new Text((snapshot.data.arrivalTime))
+                        ],
+                      ),
+                      padding: const EdgeInsets.all(25.0),
+                    ),
+                  ),
+                ]
+            );
           }
           else if (snapshot.hasError){
-            return new Text("${snapshot.error}");
+            return new Center(
+              child: new Card(
+                child: new Center(
+                  child: new Text("${snapshot.error}"),
+                ),
+              ),
+            );
           }
           return new CircularProgressIndicator();
         }
