3ae47c1dd9835e7534ed0933c6f6368372f2b7d9
Mark Madden <mark.madden@rate.com>
2018-07-12 07:56:38 -0500

    Fix drawer bug

--- lib/styles/color.dart
+++ lib/styles/color.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
  const List<CardColor> myCardColors = const <CardColor>[
-    const CardColor(const Color(0xFF565A5C), 'grey'),
     const CardColor(const Color(0xFFC60C30),  'red'),
     const CardColor(const Color(0xFF62361B), 'Brn'),
     const CardColor(const Color(0xFFF9461C), 'Org'),
@@ -10,6 +9,7 @@ import 'package:flutter/material.dart';
     const CardColor(const Color(0xFF009B3A), 'G'),
     const CardColor(const Color(0xFFE27EA6), 'Pink'),
     const CardColor(const Color(0xFFF9E300), 'Y'),
+    const CardColor(const Color(0xFF565A5C), 'grey'),
  ];
 
 
